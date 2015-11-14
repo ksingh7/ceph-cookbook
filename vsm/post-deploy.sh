301b1f6170c7c72d4419c9e607b760a3c928374a
ksingh7 <karan.singh731987@gmail.com>
2015-11-14 10:28:31 +0200

    updaded vsm/post-deploy.sh

--- vsm/post-deploy.sh
+++ vsm/post-deploy.sh
@@ -13,38 +13,16 @@ echo "
 192.168.123.102 vsm-node2
 192.168.123.103 vsm-node3
 
-
 ######################################################
 " >> /etc/hosts
 fi
-if [ -e /etc/redhat-release ]
-then
 
-	useradd cephuser
-	echo 'cephuser:cephuser' | chpasswd
-	echo "cephuser ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
-
-	systemctl stop ntpd
-	systemctl stop ntpdate
-	ntpdate 0.centos.pool.ntp.org > /dev/null 2> /dev/null
-	systemctl start ntpdate
-	systemctl start ntpd
-	yum install -y tree git epel-release
-
-	if [ -e /etc/rc.d/init.d/ceph ]
-	then
-	service ceph restart mon > /dev/null 2> /dev/null
-	fi
-
-
-	if [ $host != $VSMCONTROLLER ]
-	then
-		parted /dev/sdb -- mklabel gpt
-		parted /dev/sdc -- mklabel gpt
-		parted /dev/sdd -- mklabel gpt
-		parted -a optimal /dev/sdb -- mkpart xfs 1MB 100%
-		parted -a optimal /dev/sdc -- mkpart xfs 1MB 100%
-		parted -a optimal /dev/sdd -- mkpart xfs 1MB 100%
-	fi
-
-fi
+useradd cephuser
+echo 'cephuser:cephuser' | chpasswd
+echo "cephuser ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
+systemctl stop ntpd
+systemctl stop ntpdate
+ntpdate 0.centos.pool.ntp.org > /dev/null 2> /dev/null
+systemctl start ntpdate
+systemctl start ntpd
+yum install -y tree git epel-release
